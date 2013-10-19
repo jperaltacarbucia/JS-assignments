7bb1b24b2c6a8697f86bf3dc5bc9cb1582506cf6
Josué <palkia24@hotmail.com>
2013-10-19 11:25:48 -0400

    Operators been added

---
+++ 2013-10-20-Operators.js
@@ -0,0 +1,20 @@
+Assignment operators
+
+An assignment operator assigns a value to its left operand based on the value of its right operand. The basic assgnments operator is equal (=),
+which assigns the value of its right operand to its left operand. That is, x = y assigns the value of y to x. The other assignment operators
+are shorthand for the operations listed in the following table:
+
+
+Operator                    Meaning
+
+x += y                    x = x + y
+x -= y                    x = x - y
+x *= y                    x = x * y
+x /= y                    x = x / y
+x %= y                    x = x % y
+x <<= y                   x = x << y
+x >>= y                   x = x >> y
+x >>>= y                  x = x >>> y
+x &= y                    x = x & y
+x ^= y                    x = x ^ y
+x |= y                    x = | y
